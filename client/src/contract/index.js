ee1c435035219f96fd263e067c8cad64942331d2
Mujtaba-cpu <mobiraja1971@gmail.com>
2023-12-01 22:58:39 +0500

    final

--- client/src/contract/index.js
+++ client/src/contract/index.js
@@ -1,4 +1,4 @@
 import contract from './AVAXGods.json';
 
-export const ADDRESS = '0xb1421c7c5C2339c59e95E27594ca15F06641D59d';
+export const ADDRESS = '0xf077b93B98dc587fC9adaE482f9d1AC43cA85c4C';
 export const { abi: ABI } = contract;
